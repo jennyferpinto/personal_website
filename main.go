a67f4f620e3178a2b50d2b51e3248ea3b129f3ed
jennyferpinto <pinto.jenyfer@gmail.com>
2019-02-15 17:09:13 +0100

    fixed responsiveness of background image on mobile

--- main.go
+++ main.go
@@ -1,20 +1,72 @@
 package main
+
 import (
+	"html/template"
 	"net/http"
-	"google.golang.org/appengine" 
+	"time"
+
+	"google.golang.org/appengine"
 )
 
+//Welcome struct holds information to be displayed in our HTML file
+type Welcome struct {
+	Time string
+}
 
+//Form struct
+type Form struct {
+	Email   string
+	Subject string
+	Message string
+}
+
+//Go application entrypoint
 func main() {
-	http.HandleFunc("/", indexHandler)
-	appengine.Main() 
-}
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.Redirect(w, r, "/", http.StatusFound)
-		return
-	}
-}
+	welcome := Welcome{time.Now().Format(time.Stamp)}
 
-func contactHandler()
+	mainTemplate := template.Must(template.ParseFiles("templates/index.html"))
+
+	http.Handle("/static/",
+		http.StripPrefix("/static/",
+			http.FileServer(http.Dir("static"))))
+	// Our html code would therefore be <link rel="stylesheet"  href="/static/stylesheet/...">
+	//It is important to note the url in http.Handle can be whatever we like, so long as we are consistent.
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		//If errors show an internal server error message
+		if err := mainTemplate.ExecuteTemplate(w, "index.html", welcome); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	formTemplate := template.Must(template.ParseFiles("templates/form.html"))
+	//This method takes in the URL path "/" and a function that takes in a response writer, and a http request.
+	http.HandleFunc("/form", func(w http.ResponseWriter, r *http.Request) {
+
+		if r.Method != http.MethodPost {
+			formTemplate.Execute(w, nil)
+			return
+		}
+
+		details := Form{
+			Email:   r.FormValue("email"),
+			Subject: r.FormValue("subject"),
+			Message: r.FormValue("message"),
+		}
+
+		// do something with details
+		_ = details
+
+		formTemplate.Execute(w, struct{ Success bool }{true})
+
+	})
+
+	appengine.Main()
+
+	//Start the web server, set the port to listen to 8080. Without a path it assumes localhost
+	//Print any errors from starting the webserver using fmt
+	// fmt.Println("Listening")
+	// fmt.Println(http.ListenAndServe(":8000", nil))
+}
